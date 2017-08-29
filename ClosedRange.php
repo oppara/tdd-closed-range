8d6aa4e98aaa9bcc954a98baa35673e69c298b01
oppara <oppara@oppara.tv>
2017-08-30 02:44:23 +0900

    Refactor endpoint test

--- ClosedRange.php
+++ ClosedRange.php
@@ -2,11 +2,18 @@
 
 class ClosedRange
 {
+    private $range = [];
     public function __construct($lower_endpoint, $upper_endpoint)
     {
-        $this->lower_endpoint = $lower_endpoint;
-        $this->upper_endpoint = $upper_endpoint;
+        $this->range['lower_endpoint'] = $lower_endpoint;
+        $this->range['upper_endpoint'] = $upper_endpoint;
     }
 
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->range)) {
+            return $this->range[$name];
+        }
+    }
 }
 
