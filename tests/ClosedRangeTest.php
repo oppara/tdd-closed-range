f01e97972650646265a895748e17395ec693e911
oppara <oppara@oppara.tv>
2017-08-30 02:20:05 +0900

    Add initial RED test

---
+++ tests/ClosedRangeTest.php
@@ -0,0 +1,23 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ClosedRangeTest extends TestCase
+{
+
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function FunctionName()
+    {
+        $this->fail();
+    }
+}
+
